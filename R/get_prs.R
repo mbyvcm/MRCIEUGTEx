8eafb3e2a0194b891e786287d402ca6b8f13e325
cm16081 <cm16081@bristol.ac.uk>
2017-04-05 15:45:17 +0100

    added error checks

--- R/get_prs.R
+++ R/get_prs.R
@@ -10,15 +10,19 @@ get_query_snps_mrbase <- function(outcomes,p) {
 
 #' Given a set of dbSNP rsids, get genome coordinates (hg19) and extract these ranges from GTEx VCF
 #'
-#' @param rsids Vector of SNPs in dbSNP format.
+#' @param query Data.frame.
 #' @param gtex_vcf_dif Path to GTEx VCF file.
 #' @return Data.frame: GTEx alleles matching query.
 #' @export
-extract_query_snps_gtex <- function(rsids, gtex_vcf_dir) {
+extract_query_snps_gtex <- function(query, gtex_vcf_dir) {
 
   # check inputs exist
   if(is.null(rsids)) {stop("rsids argument required!")}
   if(is.null(rsids)) {stop("gtex_vcf_dir argument required!")}
+
+  # check query is data.frame with expected headers
+  if(!(is.data.frame(query))) {stop("query need to be a data.frame!")}
+  if(!(all(names(mrbase_query_snps) == c("SNP","effect_allele","other_allele","beta")))) {stop("query headers must be 'SNP','effect_allele','other_allele','beta'")}
 
   message(paste0("reading ",length(rsids)," rsids..."))
 
--- vignettes/MRCIEUGTEx.R
+++ vignettes/MRCIEUGTEx.R
@@ -1,3 +1,23 @@
-## ------------------------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
+#  # requires devtools
+#  install.packages('devtools')
+#  library(devtools)
+#  
+#  # install package from github
+#  install_github("mbyvcm/MRCIEUGTEx", quiet = T)
+#  library(MRCIEUGTEx)
 
+## ---- eval=FALSE---------------------------------------------------------
+#  # path to GTEx VCF file
+#  gtex_vcf_dir <- ""
+#  
+#  # path to expression & covariate tar directories downloaded from GTEx Portal
+#  covariate_matrix_tar  <- ""
+#  expression_matrix_tar <- ""
+
+## ---- eval=F-------------------------------------------------------------
+#  source('config.R')
+
+## ---- eval=FALSE---------------------------------------------------------
+#  
 
